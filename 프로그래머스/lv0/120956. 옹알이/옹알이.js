d7935ad969e6f4512227f9c9c109bd2be1dccb0f
soshin <55952886+so0112@users.noreply.github.com>
2022-10-18 15:58:41 +0900

    [level 0] Title: 옹알이, Time: 0.21 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이/옹알이.js
+++ 프로그래머스/lv0/120956. 옹알이/옹알이.js
@@ -2,5 +2,5 @@ function solution(babbling) {
     let can = babbling.map(el => el.replace(/ayaaya|yeye|woowoo|mama/g, "r")
                             .replace(/aya|ye|woo|ma/g, "")
                            );
-    return can.filter(el => el === '').length;
+    return can.filter(el => el === "").length;
 }
