0abd9f2909491e4fcbd4cea1af6f338342601aa4
soshin <55952886+so0112@users.noreply.github.com>
2022-10-24 12:52:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, num1, num2) {
+    let arr = my_string.split('');
+    let tmp = 0;
+    
+    tmp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = tmp;
+    
+    return arr.join("");
+}
