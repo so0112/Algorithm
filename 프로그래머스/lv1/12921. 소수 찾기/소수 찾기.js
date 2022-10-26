acff155fa8bea4b92138ebb68331b80ef7c1e94e
soshin <55952886+so0112@users.noreply.github.com>
2022-10-26 11:17:16 +0900

    [level 1] Title: 소수 찾기, Time: 263.64 ms, Memory: 46.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,23 @@
+// isPrime 함수 만들기
+// filter로 거르기
+
+let isPrime = function(x) {
+  let   sqrt = Math.sqrt(x);
+
+  for (let i = 2; i <= sqrt; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(n) {
+  let   arr = [2];
+
+  for (let i = 3; i <= n; i += 2) {
+    arr.push(i);
+  }
+  arr = arr.filter((el) => {return isPrime(el) === true});
+  return arr.length;
+}
