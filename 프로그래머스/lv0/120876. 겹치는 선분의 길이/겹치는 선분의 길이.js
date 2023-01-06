fcd4fb343f1ccf18fcac250dba85a1719dc0a6f2
soshin <55952886+so0112@users.noreply.github.com>
2023-01-06 19:03:42 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,20 +1,14 @@
 function solution(lines) {
-    let line = {}
-    let answer = 0;
+    const obj = {};
     
-    for(let i = 0; i < lines.length; i++){
-        for(let j = lines[i][0]; j < lines[i][1]; j++){
-            if (line[j]) {
-                if(line[j][1] !== 0) {
-                    answer += 1;
-                    line[j] = [j, 0]
-                }
+    for (let i = 0; i < lines.length; i++){
+        for (let j = lines[i][0]; j < lines[i][1]; j++) {
+            if (obj[j]) {
+                obj[j] = "o"
             } else {
-                line[j]=[0]    
+                obj[j] = "x"
             }
-
         }
     }
-    console.log(line);
-    return answer;
+    return Object.values(obj).filter((el) => el === "o").length;
 }
