e3eb6d47ed81264911a8d01905111513493914df
soshin <55952886+so0112@users.noreply.github.com>
2023-01-10 22:40:59 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+//* 인풋 - 커스텀, 함수 콜
+const nums = input.map((num) => Number(num));
+sol(nums);
+
+//* 로직함수
+function sol(nums) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], 1);
+  }
+
+  for (let i = 1; i <= 30; i++) {
+    if (!map.has(i)) {
+      console.log(i);
+    }
+  }
+}
