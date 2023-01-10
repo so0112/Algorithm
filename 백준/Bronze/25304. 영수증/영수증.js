991e53c514ce4f58e5239d125ea209931334a941
soshin <55952886+so0112@users.noreply.github.com>
2023-01-10 22:21:25 +0900

    [Bronze V] Title: 영수증, Time: 140 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const result = Number(input[0]);
+
+const count = Number(input[1]);
+
+input.splice(0, 2);
+
+let check = 0;
+
+for (let i = 0; i < count; i++) {
+  let [price, num] = input[i].split(" ");
+  check += Number(price) * Number(num);
+}
+
+if (result === check) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
