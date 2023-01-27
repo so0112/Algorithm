2724adcdebe38902a6a2a20e9a8f260361f5d188
soshin <55952886+so0112@users.noreply.github.com>
2023-01-27 10:12:10 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.js
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -1,23 +1,41 @@
-const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString();
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString();
 
-let dial = {
-    ABC: 3,
-    DEF: 4,
-    GHI: 5,
-    JKL: 6,
-    MNO: 7,
-    PQRS: 8,
-    TUV: 9,
-    WXYZ: 10,
+let result = 0;
+const obj = {
+  A: 3,
+  B: 3,
+  C: 3,
+  D: 4,
+  E: 4,
+  F: 4,
+  G: 5,
+  H: 5,
+  I: 5,
+  J: 6,
+  K: 6,
+  L: 6,
+  M: 7,
+  N: 7,
+  O: 7,
+  P: 8,
+  Q: 8,
+  R: 8,
+  S: 8,
+  T: 9,
+  U: 9,
+  V: 9,
+  W: 10,
+  X: 10,
+  Y: 10,
+  Z: 10,
 };
 
-let minTime = 0;
-for (let i=0; i<input.length; i++) {
-    for (let x in dial) {
-        if(x.includes(input[i])) {
-            minTime += dial[x];
-        }
-    }
+for (let i = 0; i < input.length; i++) {
+  if (obj[input[i]]) {
+    result += obj[input[i]];
+  }
 }
-console.log(minTime);
+
+console.log(result);
