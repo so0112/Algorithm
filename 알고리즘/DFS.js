723a7f96c8afea6fb4f01931bd3c86187a3496c8
so0112 <tkddh0112@gmail.com>
2023-02-24 00:51:07 +0900

    Refactor: DFS

---
+++ 알고리즘/DFS.js
@@ -0,0 +1,30 @@
+const graph = {
+  A: ["B", "C"],
+  B: ["A", "D"],
+  C: ["A", "G", "H", "I"],
+  D: ["B", "E", "F"],
+  E: ["D"],
+  F: ["D"],
+  G: ["C"],
+  H: ["C"],
+  I: ["C", "J"],
+  J: ["I"],
+};
+
+const BFS = (graph, startNode) => {
+  const visited = [];
+  const needVisit = [];
+
+  needVisit.push(startNode);
+
+  while (needVisit.length !== 0) {
+    const node = needVisit.pop();
+    if (!visited.includes(node)) {
+      visited.push(node);
+      needVisit.push(...graph[node].reverse());
+    }
+  }
+  return visited;
+};
+
+console.log(BFS(graph, "A"));
