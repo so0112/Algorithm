2b62e76b1681530f24f31c36e8e2b310c2069251
soshin <55952886+so0112@users.noreply.github.com>
2023-02-24 02:21:25 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,16 +1,14 @@
 function solution(number) {
-    let result = 0;
-
-    const combination = (current, start) => {
-        if (current.length === 3) {
-            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
-            return;
-        }
-
-        for (let i = start; i < number.length; i++) {
-            combination([...current, number[i]], i + 1);
+   let count = 0;
+    
+    for (let i = 0; i < number.length - 2; i++) {
+        for (let j = i + 1; j < number.length - 1; j++) {
+            for (let k = j + 1; k < number.length; k ++) {
+                let sum = number[i] + number[j] + number[k];
+                if (sum === 0) count++;
+            }
         }
     }
-    combination([], 0);
-    return result;
+    
+    return count;
 }
