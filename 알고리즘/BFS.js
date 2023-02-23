c1d24459e2f29200bceb9808ca08bcaecdd93eee
so0112 <tkddh0112@gmail.com>
2023-02-24 00:43:15 +0900

    Refactor: unshift -> push 변경

--- 알고리즘/BFS.js
+++ 알고리즘/BFS.js
@@ -12,18 +12,16 @@ const graph = {
 };
 
 const BFS = (graph, startNode) => {
-  const visited = []; // 탐색을 마친 노드들
-  let needVisit = []; // 탐색해야할 노드들
+  const visited = [];
+  const needVisit = [];
 
-  needVisit.push(startNode); // 노드 탐색 시작
-  console.log(needVisit);
+  needVisit.push(startNode);
+
   while (needVisit.length !== 0) {
-    // 탐색해야할 노드가 남아있다면
-    const node = needVisit.shift(); // queue이기 때문에 선입선출, shift()를 사용한다.
+    const node = needVisit.pop();
     if (!visited.includes(node)) {
-      // 해당 노드가 탐색된 적 없다면
       visited.push(node);
-      needVisit = [...needVisit, ...graph[node]];
+      needVisit.push(...graph[node].reverse());
     }
   }
   return visited;
