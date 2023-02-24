dfc1f7026c4aec4ca2880da2495366b6b4d50afd
soshin <55952886+so0112@users.noreply.github.com>
2023-02-24 16:05:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -3,14 +3,14 @@ function solution(n, words) {
 
   for (let i = 0; i < words.length; i++) {
     let word = words[i];
-    let p = i % n + 1;
+    let player = i % n + 1;
     let turn = Math.ceil((i + 1)/n);
 
     if (i > 0) {
       let last = words[i - 1].split("").pop();
 
-      if (i > words.indexOf(word) || words[i][0] !== last) {
-        answer = [p, turn];
+      if (i !== words.indexOf(word) || words[i][0] !== last) {
+        answer = [player, turn];
         break;
       }
     }
