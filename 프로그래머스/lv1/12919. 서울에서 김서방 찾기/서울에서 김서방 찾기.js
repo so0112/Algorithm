a63aecf3ac618c16f43b72a9be577097e42ba747
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-08 00:22:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,3 @@
 function solution(seoul) {
-    for (let i = 0; i < seoul.length; i++) {
-        if (seoul[i] === "Kim") return `김서방은 ${i}에 있다`
-    }
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`
 }
