33402d177ea2851b79ffcd0beba13449b0dbbc16
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-08 00:33:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,15 +1,6 @@
-// 번호 뒷 4자리를 제외한 모든 번호 '*' 값으로 변경 
-// len 8 일경우 7 6 5 4
-// 문자.length - 5 까지는 싹 다 * 값으로 변경해라
 function solution(phone_number) {
-    var answer = '';
-    let len = phone_number.length
-    for (let i = 0; i < len - 4; i++){
-        answer += '*'
-    }
-    for (let j = len - 4; j < len; j++){
-        answer += phone_number[j];
-    }
-
-    return answer;
+    let numLen = phone_number.length;
+    let num = phone_number.slice(-4)
+    
+    return "*".repeat(numLen - 4) + num;
 }
