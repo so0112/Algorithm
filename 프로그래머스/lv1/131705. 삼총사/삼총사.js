a3bad763eb93c66bdffa047c164e0bbcc6802edc
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-08 21:37:32 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,14 +1,12 @@
 function solution(number) {
-   let count = 0;
+    let cnt = 0;
     
-    for (let i = 0; i < number.length - 2; i++) {
-        for (let j = i + 1; j < number.length - 1; j++) {
-            for (let k = j + 1; k < number.length; k ++) {
-                let sum = number[i] + number[j] + number[k];
-                if (sum === 0) count++;
+    for (let i = 0; i < number.length; i++) {
+        for (let j = i + 1; j < number.length; j++) {
+            for (let k = j + 1; k < number.length; k++) {
+                if (number[i] + number[j] + number[k] === 0) cnt++;
             }
         }
     }
-    
-    return count;
+    return cnt;
 }
