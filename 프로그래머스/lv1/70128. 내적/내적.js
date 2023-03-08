eb4cb70b315557721682efa5dc63e4f9dd220422
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-08 10:32:15 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,9 +1,3 @@
 function solution(a, b) {
-    let result = 0;
-    
-    for (let i = 0; i < a.length; i++) {
-        result += a[i] * b[i];
-    }
-    
-    return result;
+    return a.reduce((acc, cur, idx) => acc += a[idx] * b[idx], 0)
 }
