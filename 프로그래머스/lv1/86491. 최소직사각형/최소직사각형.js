682fd47da6db5873682a34b4084906f7b2b7666f
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-08 22:34:15 +0900

    [level 1] Title: 최소직사각형, Time: 5.68 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,7 +1,12 @@
 function solution(sizes) {
-    let arr = sizes.map(el => el.sort((a, b) => b - a));
-    let width = Math.max(...arr.map(el => el[0]));
-    let height = Math.max(...arr.map(el => el[1]));
+    let [w, h] = [0, 0];
+    let sort = sizes.map((el) => el.sort((a, b) => a - b));
     
-    return width * height  
+    
+    for (let i = 0; i < sort.length; i++) {
+        if (w < sort[i][0]) w = sort[i][0];
+        if (h < sort[i][1]) h = sort[i][1];
+    }
+    
+    return w * h;
 }
