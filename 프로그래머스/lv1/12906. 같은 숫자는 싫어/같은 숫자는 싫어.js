d5c599b82af34c1a7818a3447bc7f871863c83a9
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-08 14:16:25 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.31 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr){
+    let result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (result[result.length - 1] !== arr[i]) result.push(arr[i]);
+    }
+    return result;
+}
