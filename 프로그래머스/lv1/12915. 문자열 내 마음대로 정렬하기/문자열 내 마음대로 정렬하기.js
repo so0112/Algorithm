ac9bb030f26c4bf610e64948bf172f508de4f8df
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-10 11:00:50 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,14 @@
+function solution(strings, n) {
+
+    strings.sort((a, b) => {
+        if(a[n] > b[n]) return 1;
+        else if (a[n] < b[n]) return -1;
+        
+        if (a > b) return 1;
+        else if (a < b) return -1;
+        
+        return 0;
+    });
+    
+    return strings;
+}
