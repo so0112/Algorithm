f94b2be8cac5442b89cc9104f8a199bbef08d502
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-10 11:35:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -2,7 +2,7 @@ function solution(s) {
     const arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
     for (let i = 0; i < arr.length; i++){
-        s = s.replaceAll(arr[i], i);
+        s = s.split(arr[i]).join(i);
     }
     return Number(s);
 }
