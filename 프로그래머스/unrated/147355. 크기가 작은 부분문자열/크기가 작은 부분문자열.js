b83e2174b07b41505074ba9394abca16e264e01c
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-11 01:27:38 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,14 +1,11 @@
 function solution(t, p) {
-    
-    let result = [];    
-    // len와 길이가 같은 부분 문자열 만들기
-    let list = [];
+    let cnt = 0;
     let len = p.length;
 
     for (let i = 0; i <= t.length - len; i++) {
         let num = t.slice(i, i + len);
-        list.push(num);
+        if (num <= p) cnt++;
     }
     
-    return list.filter((el) => el <= p).length;
+    return cnt;
 }
