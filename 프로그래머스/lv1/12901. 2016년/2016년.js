eb775823316e6b31415b246bc88b6004a7a77a82
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-12 01:38:31 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,33 +1,15 @@
-// 1월 1일은 FRI
-
-
-// 월 - 일
-// 1 - 31
-// 2 - 28
-// 3 - 31
-// 4 - 30
-// 5 - 31
-// 6 - 30
-// 7 - 31
-
-// 8 - 31
-// 9 - 30
-// 10 - 31
-// 11 - 30
-// 12 - 31
-
-
 function solution(a, b) {
-
-    const   day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
-    const   mon = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    let     result = 0;
     
-    for (let i = 0; i < a - 1; i++){
-        result += mon[i];
+    // 월 일수 배열 (인덱스 맞추기용 0번 인덱스)
+    let numbers = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+    
+    // (a - 1월까지 일 수 + b) % 7 => 요일
+    let cnt = b;
+    
+    for (let i = 1; i < a; i++) {
+        cnt += numbers[i];
     }
     
-    result += b;
-    
-    return day[result % 7];
+    return days[cnt % 7];
 }
