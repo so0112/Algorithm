26cda3b078a1b9d42e046fd2834c99cce3bde56b
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-12 02:31:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,6 @@
 function solution(nums) {
     let num = nums.length / 2;
-    let set = new Set(nums);
-    let setLen = [...set].length;
+    let set = new Set(nums).size;
     
-    return num < setLen ? num : setLen;
+    return num < set ? num : set;
 }
