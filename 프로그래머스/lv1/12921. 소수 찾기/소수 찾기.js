399e2f7cfef712da73b724ac89e681c52b36e02b
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-12 15:03:14 +0900

    [level 1] Title: 소수 찾기, Time: 122.48 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,23 +1,17 @@
-// isPrime 함수 만들기
-// filter로 거르기
-
-let isPrime = function(x) {
-  let   sqrt = Math.sqrt(x);
-
-  for (let i = 2; i <= sqrt; i++) {
-    if (x % i === 0) {
-      return false;
+// 소수 판별 함수
+function findPrimeNum(num) {
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0 || num % 2 === 0) return false;
     }
-  }
-  return true;
+    return true;
 }
 
 function solution(n) {
-  let   arr = [2];
-
-  for (let i = 3; i <= n; i += 2) {
-    arr.push(i);
-  }
-  arr = arr.filter((el) => {return isPrime(el) === true});
-  return arr.length;
+    if (n < 2) return 0;
+    
+    let cnt = 1;
+    for (let i = 3; i <= n; i += 2) {
+        if(findPrimeNum(i)) cnt++;
+    }
+    return cnt;
 }
