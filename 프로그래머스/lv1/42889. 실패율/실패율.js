41969ee9e9bb05c70090dd1f0e71c625d4672fcd
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-12 17:52:38 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,23 +1,17 @@
-// N : 최대 스테이지
-// stages : 플레이어들이 현재 어떤 스테이지에 있는 지 담긴 배열
+// 실패율 === 스테이지 도달 했는데 클리어 못 한 사람 수 / 스테이지 도달한 플레이어
 
-// N까지 반복문을 돌면서 각 스테이지를 얼만큼 넘겼나 확인 객체 값으로 담기
-
-
-const   result = [];
-
+// 실패율 높은 스테이지부터 내림차순 배열 반환
 function solution(N, stages) {
     
+    let FailureRate = [];
     
-    for (let i = 1; i <= N; i++){
-        
-       let curStage = stages.filter((el) => el === i).length;
-       let passStage = stages.filter((el) => el >= i).length;
-        
-        result.push([i, (curStage / passStage)])
+    for (let i = 0; i < N; i++) {
+        let players = stages.filter(el => el >= i + 1).length;
+        let stoppedPlayers = stages.filter(el => el === i + 1).length;
+        FailureRate.push([i + 1, stoppedPlayers / players]);
     }
-    console.log(result)
-    result.sort((a, b) => b[1] - a[1]);
     
-    return result.map((el) => el[0]);
+    FailureRate.sort((a, b) => b[1] - a[1]);
+    
+    return FailureRate.map(el => el[0]);
 }
