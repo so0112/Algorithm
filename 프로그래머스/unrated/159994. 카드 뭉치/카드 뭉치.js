19818896d68e4971ff698bca9635c192377b4df5
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-14 00:03:50 +0900

    [unrated] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal){
+    
+    while (goal.length > 0) {
+
+        if (goal[0] === cards1[0]) {
+            goal.shift();
+            cards1.shift();
+        } else if (goal[0] === cards2[0]){
+            goal.shift();
+            cards2.shift();
+        } else {
+            return "No";
+        }
+    }
+    
+    return "Yes";
+}
