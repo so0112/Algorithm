7dcd2b0093216103bcd62b2437dfd118d821eab7
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-03-13 20:47:01 +0900

    [level 1] Title: 체육복, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,33 +1,36 @@
-// n : 전체 학생수
-// lost : 도난 당한 학생의 번호
-// reserve : 여벌 체육복을 가져온 학생들의 번호
-
-// return 체육복 입은 학생의 최대값
-
-// 조건
-// 여벌 가져온 학생들도 도난 당할 수 있음 -> 여벌이있어도 도난을 당했다면 빌려줄 수 없다
-// 인접한 번호의 학생들에게만 빌려줄 수 있다.
 function solution(n, lost, reserve) {
-    for(let item of lost){
-        if (reserve.includes(item)){
-           reserve = reserve.filter((el) => el !== item);
-           lost = lost.filter((el => el !== item))
+    
+    // 도난 이전 
+    const arr = [];
+
+    for (let i = 0; i < n; i++) {
+        arr.push(1);
+        arr[i] -= 1;
+    }
+    
+    // 도난 이후
+    for (let i = 0; i < lost.length; i++) {
+        arr[lost[i] - 1] -= 1;
+    }
+    
+    // 여벌 보유
+    for (let i = 0; i < reserve.length; i++) {
+        arr[reserve[i] - 1] += 1;
+    }
+    
+    // 대여
+    for (let i = 0; i < n; i++) {
+        if (arr[i] === 1 && arr[i + 1] === -1) {
+            arr[i] = 0;
+            arr[i + 1] = 0;
+        }
+        
+        if (arr[i] === -1 && arr[i + 1] === 1) {
+            arr[i] = 0;
+            arr[i + 1] = 0;
         }
     }
-    let cnt = n - lost.length
-     
-    for (let i = 1; i <= n; i++){
-        if (lost.includes(i)){
-            if (reserve.includes(i - 1)){
-                lost = lost.filter((el) => el !== i)
-                reserve = reserve.filter((el) => el !== i - 1)
-                cnt++;
-            } else if (reserve.includes(i + 1)){
-                lost = lost.filter((el) => el !== i)
-                reserve = reserve.filter((el) => el !== i + 1)
-                cnt++;
-            }
-        }
-    }
-    return cnt;
+
+    
+    return n - arr.filter(el => el === -1).length;
 }
