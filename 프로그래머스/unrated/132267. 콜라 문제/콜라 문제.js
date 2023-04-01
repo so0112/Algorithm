85171fccd04270cefb96413b7b3198a8bf0f414a
Sangoh Shin <55952886+so0112@users.noreply.github.com>
2023-04-01 23:20:44 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,15 +1,11 @@
 function solution(a, b, n) {
-    // 빈 병 n 개 있음
-    // 빈 병 a 개 가져가면 콜라 b 병 준다
     
-    let result = 0;
+    let bottle = 0
     
-    while (n >= a) {
-        let newCoke = Math.floor(n / a) * b;
-        n = n % a;
-        n += newCoke;
-        result += newCoke;
+    while(n >= a) {
+        let change = Math.floor(n / a) * b;
+        bottle += change
+        n = change + n % a
     }
-    
-    return result;
+    return bottle
 }
