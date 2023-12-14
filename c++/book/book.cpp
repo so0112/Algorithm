b9d26092f52cb1314dda8fadda229e2c2d14664e
so0112 <tkddh0112@gmail.com>
2023-12-15 00:02:13 +0900

    [1강] c++ 시간복잡도

---
+++ c++/book/book.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n, cnt;
+
+int main() {
+  cin >> n;
+  int a = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      a += i + j;
+      cnt++;
+    }
+  }
+  cout << a << '\n'; // Fixed the typo here
+  cout << " cnt : " << cnt << '\n';
+  return 0;
+}
